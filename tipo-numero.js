ceb007aa7a35e90b04d07a0ab6003b6fa8e4d717
Camila <zanellacamila016@gmail.com>
2024-03-13 12:34:40 -0300

    js

---
+++ tipo-numero.js
@@ -0,0 +1,17 @@
+//numeros inteiros
+const primeiroNumero=10;
+const segundoNumero=20;
+
+console.log(primeiroNumero+segundoNumero);
+
+//ponto flutuante
+const primeiroDecimal=3.14;
+const segundoDecimal=.5;
+
+console.log(primeiroDecimal/segundoDecimal);
+console.log(primeiroDecimal-segundoDecimal);
+console.log(primeiroDecimal*segundoDecimal);
+
+const divisao=primeiroDecimal/segundoDecimal;
+console.log(divisao.toFixed(1));
+
---
+++ tipo-texto.js
@@ -0,0 +1,9 @@
+const nomeAluno="Beatriz";
+const maeAluno=" Juliana";
+
+console.log(nomeAluno);
+
+const idadeAluno=17;
+
+console.log(nomeAluno*idadeAluno);
+console.log(nomeAluno+maeAluno); //concatenação
